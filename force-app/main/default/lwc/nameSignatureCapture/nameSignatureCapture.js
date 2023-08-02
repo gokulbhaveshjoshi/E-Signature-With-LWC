fe220bbb9d423225c24269cf0258db792870a5ec
Bhavesh Joshi <34483890+gokulbhaveshjoshi@users.noreply.github.com>
2023-08-02 16:56:29 +0530

    Added nameSignature Component

---
+++ force-app/main/default/lwc/nameSignatureCapture/nameSignatureCapture.js
@@ -0,0 +1,44 @@
+/**
+ * @description       : 
+ * @author            : Bhavesh Joshi
+ * @group             : 
+ * @last modified on  : 08-02-2023
+ * @last modified by  : Bhavesh Joshi
+**/
+import { LightningElement } from 'lwc';
+
+export default class NameSignatureCapture extends LightningElement {
+    imgSrc;
+    
+    renderedCallback() {
+        document.fonts.forEach((font) => {
+            if (font.family === "Great Vibes" && font.status === "unloaded") {
+                // Ensure that the font is loaded so that signature pad could use it.
+                // If you are using a different font in your project, don't forget
+                // to update the if-condition above to account for it.
+                font.load();
+            }
+        });
+    }
+    
+    saveSignature() {
+        const pad = this.template.querySelector("c-signature-pad");
+        if (pad) {
+            const dataURL = pad.getSignature();
+            if (dataURL) {
+                // At this point you can consume the signature, for example by saving
+                // it to disk or uploading it to a Salesforce org/record.
+                // Here we just preview it in an image tag.
+                this.imgSrc = dataURL;
+            }
+        }
+    }
+    
+    clearSignature() {
+        const pad = this.template.querySelector("c-signature-pad");
+        if (pad) {
+            pad.clearSignature();
+        }
+        this.imgSrc = null;
+    }
+}
